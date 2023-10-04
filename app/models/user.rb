fcc0c8a64fd98dbb4e599368fd32c561ace73e42
kajigii <wata15708@gmail.com>
2023-10-04 21:25:45 +0900

    itemモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :date_of_birth, presence: true
+
+  has_many :items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'items#index'
+  resources :users
   resources :items
 end
