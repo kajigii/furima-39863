0d727e6f9cc66b25d0199943772b3de54641f493
kajigii <wata15708@gmail.com>
2023-10-05 12:06:28 +0900

    カテゴリー情報の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :region
+  belongs_to :until_shipping
+
+
+  validates :item_name, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :burden_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
